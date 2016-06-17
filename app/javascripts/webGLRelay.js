0e4c93b8bb83cd9d5ff8e08c0eff849893c64209
Veronica Moreno <frau.veronika@gmail.com>
2016-06-17 00:30:40 -0700

    livin and learnin x2

--- app/javascripts/webGLRelay.js
+++ app/javascripts/webGLRelay.js
@@ -1,5 +1,5 @@
-function relayImgurData(data) {
+window.relayImgurData = function(data) {
   console.log(typeof data + ' ' + data);
   console.log(JSON.parse(data));
-  window.addPicture(JSON.parse(data));
+  addPicture(JSON.parse(data));
 }
